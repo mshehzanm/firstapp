e031d8601f2747c081b6f235c2e47ab14b37eba2
M shehzan <mshehzanm@live.com>
2018-08-12 15:35:23 +0530

    first commit

---
+++ app/src/main/java/com/example/shehzan/libapplication/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.example.shehzan.libapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME="";
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
